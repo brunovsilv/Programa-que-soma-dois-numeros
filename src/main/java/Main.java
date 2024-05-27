8e4cf0efd95b6f2f31b394c58e6afe5d0525baac
Bruno Vinicius da Silva Corsini <169068441+brunovsilv@users.noreply.github.com>
2024-05-27 12:27:29 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Programa que soma dois números");
+System.out.println("o primeiro número é:");
+    int numero1 = 4;
+    
+      System.out.println(numero1);
+    System.out.println("o segundo número é:");
+    int numero2 = 9;
+
+      System.out.println(numero2);
+      System.out.println("o resultado é");
+    int soma = numero1 + numero2;
+    System.out.println(soma);
+    
+    
+                      
+  }
+  ;
+}
